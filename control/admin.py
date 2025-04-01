c4ea1e3728d05fa417fab34b99fc39fc11e45492
Joaquín Caparros <jj.caparros@alumno.um.edu.ar>
2025-04-01 20:54:40 -0300

    Modificación de models, agregados a admin, y migraciones a mysql

--- control/admin.py
+++ control/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Empresa)
+admin.site.register(Sucursal)
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(StockSucursal)
+admin.site.register(MovimientoStock)
+
--- stockify/settings.py
+++ stockify/settings.py
@@ -76,10 +76,24 @@ WSGI_APPLICATION = 'stockify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stockify_db',  
+        'USER': 'root',         
+        'PASSWORD': 'joaco711',  
+        'HOST': 'localhost',          
+        'PORT': '3306',               
+        'OPTIONS': {
+            'charset': 'utf8mb4',  
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
