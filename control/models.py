e43377ec038e846cb6990bf6f2f5ee48f8200ba1
Joaquín Caparros <jj.caparros@alumno.um.edu.ar>
2025-05-08 14:27:17 -0300

    Now the fields address and phone, canoot be null or blank in model Branch

--- control/models.py
+++ control/models.py
@@ -18,8 +18,8 @@ class Company(models.Model):
 class Branch(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255, null=True, blank=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
