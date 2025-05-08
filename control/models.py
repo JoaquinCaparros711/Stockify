c9249d7fc63d320131a733a3af63a9db52dce446
Joaquín Caparros <jj.caparros@alumno.um.edu.ar>
2025-05-08 16:12:19 -0300

    Changes in Company models

--- control/models.py
+++ control/models.py
@@ -6,8 +6,8 @@ class Company(models.Model):
     name = models.CharField(max_length=255)
     cuit = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
-    address = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
     registration_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
