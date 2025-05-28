a4f0e7e62bc914b82632aef8d69b475119ab0d9b
Joaquín Caparros <jj.caparros@alumno.um.edu.ar>
2025-05-28 18:37:56 -0300

    modified urls with rest_framework_simplejwt

--- user_control/urls.py
+++ user_control/urls.py
@@ -1,17 +1,33 @@
-from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 from django.urls import path, include
-from user_control import views
-from user_control import views
+from user_control.views import *
+
+#from .views import UserView, current_user
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 router = routers.DefaultRouter()
-router.register(r'register', views.UserView, basename='register')
+router.register(r'register', UserView, basename='register')
 
 # Sin usar router para estas views "manuales"
+# urlpatterns = [
+#     path('register/', include(router.urls)),
+#     path('login/', views.LoginView.as_view(), name='login'),
+#     path('logout/', views.LogoutView.as_view(), name='logout'),
+#     path('admin/create-user/', views.CreateUserByAdminView.as_view(), name='admin-create-user'),
+#     path('admin/create-branch/', views.CreateBranchByAdminView.as_view(), name='admin-create-branch'),
+# ]
+
 urlpatterns = [
+    #path('', include(router.urls)),  # incluye las rutas de UserView
     path('register/', include(router.urls)),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('admin/create-user/', views.CreateUserByAdminView.as_view(), name='admin-create-user'),
-    path('admin/create-branch/', views.CreateBranchByAdminView.as_view(), name='admin-create-branch'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    #path('me/', current_user, name='current_user'),
 ]
